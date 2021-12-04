ff129066973751bf5f7ccacde19f17295223035e
Chris Athanas <chris@crowdskout.com>
2021-12-03 19:24:41 -0600

    answers Day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,20 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.windowed(2).count { (a, b) -> a < b }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input
+            .windowed(4)
+            .count {
+                it[0] < it[3]
+            }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    println(part1(testInput))
+    check(part1(testInput) == 7)
 
     val input = readInput("Day01")
     println(part1(input))
